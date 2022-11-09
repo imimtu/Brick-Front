bf59a34eab92fee845dbe6026ddcab5ffd8221fe
안강민 <hkkm612@gmail.com>
2022-11-10 02:25:12 +0900

    Platform 구분 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
+// Distinguish Platform : https://stackoverflow.com/a/50744481
+// import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:brick/pages/pages.dart';
 import 'package:brick/utils/utils.dart';
 
 void main() {
-  runApp(const App());
+  if (kIsWeb) {
+    runApp(const WebApp());
+  } else {
+    runApp(const MobileApp());
+  }
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MobileApp extends StatelessWidget {
+  const MobileApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Brick',
+      debugShowCheckedModeBanner: false,
+      theme: basicTheme,
+      home: const HomePage(),
+    );
+  }
+}
+
+class WebApp extends StatelessWidget {
+  const WebApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Brick Web',
       debugShowCheckedModeBanner: false,
       theme: basicTheme,
       home: const HomePage(),

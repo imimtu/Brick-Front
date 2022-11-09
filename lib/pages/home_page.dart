bb02384e127f834a7c416a1fd122528fa5a0bd4d
안강민 <hkkm612@gmail.com>
2022-11-10 02:15:51 +0900

    빈 Drawer추가

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,6 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      drawer: Drawer(),
       body: const Center(
         child: Text("Home Page"),
       ),

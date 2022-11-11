1ae78b864673c90157b3cc05b9fc34af77566c60
안강민 <hkkm612@gmail.com>
2022-11-11 22:18:21 +0900

    Home UI 구조 다듬기

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,16 +11,81 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(),
-      body: const Center(
-        child: Text("Home Page"),
+      appBar: _appBar(context),
+      drawer: _drawer(context),
+      body: _body(context),
+      floatingActionButton: _floatingButton(context),
+    );
+  }
+
+  /// Home - AppBar
+  PreferredSizeWidget _appBar(BuildContext context) {
+    return AppBar(
+      leading: Builder(builder: (context) {
+        return IconButton(
+          tooltip: "Drawer Button",
+          icon: const Icon(
+            Icons.menu,
+          ),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+        );
+      }),
+      title: const Text("Brick"),
+    );
+  }
+
+  /// Home - Drawer
+  Widget _drawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: const EdgeInsets.all(0),
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text("Drawer"),
+          ),
+          ListTile(
+            title: const Text("Menu1"),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text("Menu2"),
+            onTap: () {},
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Additional Button',
-        child: const Icon(Icons.add_task),
+    );
+  }
+
+  /// Home - Body
+  Widget _body(BuildContext context) {
+    DateTime now = DateTime.now();
+    String today =
+        "${now.year}.${now.month < 10 ? '0${now.month}' : now.month}.${now.day < 10 ? '0${now.day}' : now.day}";
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [Text(today)],
+          )
+        ],
       ),
+    );
+  }
+
+  /// Home - floatingButton
+  Widget _floatingButton(BuildContext context) {
+    return FloatingActionButton(
+      tooltip: 'Additional Button',
+      child: const Icon(Icons.add_task),
+      onPressed: () {},
     );
   }
 }

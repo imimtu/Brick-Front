b06ac63cbaea46c9d17c8b23281f062a5256a834
안강민 <hkkm612@gmail.com>
2023-01-24 21:11:54 +0900

    Data Layer Class 생성
    - Brick Remote & Local Repository
    - User Remote & Local Repository

---
+++ lib/src/data/repositories/user/user_repository_remote.dart
@@ -0,0 +1,28 @@
+import 'package:brick/src/domain/entities/user.dart';
+import 'package:brick/src/domain/repositories/user_repository.dart';
+
+class UserRepositoryRemote implements UserRepository {
+  @override
+  Future create(User user) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  Future delete(User user) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future read(User user) {
+    // TODO: implement read
+    throw UnimplementedError();
+  }
+
+  @override
+  Future update(User user) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+}

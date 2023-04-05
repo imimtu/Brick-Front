f3eb2cafe183ae4cb03b3501835b749738382941
안강민 <hkkm612@gmail.com>
2023-04-06 01:01:46 +0900

    feat. [Config] SharedPreference 추가

---
+++ lib/src/config/local_storage/local_storage_config.dart
@@ -0,0 +1,101 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum LocalStorageKey {
+  token,
+  refreshToken,
+}
+
+class LocalStorageConfig {
+  LocalStorageConfig._();
+
+  static final LocalStorageConfig _instance = LocalStorageConfig._();
+
+  factory LocalStorageConfig() {
+    return _instance;
+  }
+
+  late SharedPreferences prefs;
+  late String envPrefix;
+
+  String _getKey(LocalStorageKey localStorageKey) =>
+      "${envPrefix}_${localStorageKey.name}";
+
+  Future init({required String env}) async {
+    prefs = await SharedPreferences.getInstance();
+    envPrefix = "${env}_";
+  }
+
+  Set<String> getKeys() {
+    return prefs.getKeys();
+  }
+
+  Object? getValue(LocalStorageKey localStorageKey) {
+    String key = _getKey(localStorageKey);
+    return prefs.get(key);
+  }
+
+  bool? getValueBool(LocalStorageKey localStorageKey) {
+    String key = _getKey(localStorageKey);
+    return prefs.getBool(key);
+  }
+
+  double? getValueDouble(LocalStorageKey localStorageKey) {
+    String key = _getKey(localStorageKey);
+    return prefs.getDouble(key);
+  }
+
+  int? getValueInt(LocalStorageKey localStorageKey) {
+    String key = _getKey(localStorageKey);
+    return prefs.getInt(key);
+  }
+
+  String? getValueString(LocalStorageKey localStorageKey) {
+    String key = _getKey(localStorageKey);
+    return prefs.getString(key);
+  }
+
+  List<String>? getValueStringList(LocalStorageKey localStorageKey) {
+    String key = _getKey(localStorageKey);
+    return prefs.getStringList(key);
+  }
+
+  setValueBool({
+    required LocalStorageKey localStorageKey,
+    required bool val,
+  }) {
+    String key = _getKey(localStorageKey);
+    prefs.setBool(key, val);
+  }
+
+  setValueDouble({
+    required LocalStorageKey localStorageKey,
+    required double val,
+  }) {
+    String key = _getKey(localStorageKey);
+    prefs.setDouble(key, val);
+  }
+
+  setValueInt({
+    required LocalStorageKey localStorageKey,
+    required int val,
+  }) {
+    String key = _getKey(localStorageKey);
+    prefs.setInt(key, val);
+  }
+
+  setValueString({
+    required LocalStorageKey localStorageKey,
+    required String val,
+  }) {
+    String key = _getKey(localStorageKey);
+    prefs.setString(key, val);
+  }
+
+  setValueStringList({
+    required LocalStorageKey localStorageKey,
+    required List<String> val,
+  }) {
+    String key = _getKey(localStorageKey);
+    prefs.setStringList(key, val);
+  }
+}

a09d8ba8a0bb1c2c1fafc3b35a57f86e79ddffb0
안강민 <hkkm612@gmail.com>
2023-05-29 23:54:07 +0900

    task. [View / Page / Splash] Splash View 작업

--- lib/src/app/pages/login/login.dart
+++ lib/src/app/pages/login/login.dart
@@ -1,6 +1,9 @@
+import 'package:brick/src/app/app.dart';
 import 'package:brick/src/app/pages/home/home.dart';
 import 'package:brick/src/app/pages/signup/signup.dart';
 import 'package:brick/src/app/providers/auth_provider.dart';
+import 'package:brick/src/app/styles/ddl/ddl.dart';
+import 'package:brick/src/util/brick_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -16,30 +19,122 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final Key _formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailTextCtrl = TextEditingController();
+  final TextEditingController passwordTextCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    double bodyWidth = 300;
+
+    double gapUnit = 8;
+
+    var brightness = MediaQuery.of(context).platformBrightness;
+    bool isDarkMode = brightness == Brightness.dark;
+    Color textColor = BrickColors.black;
+
+    if (isDarkMode) {
+      textColor = BrickColors.white;
+    }
+
+    return WillPopScope(
+      onWillPop: () async {
+        // Android 뒤로가기 버튼 무효 처리.
+        return false;
+      },
       child: Scaffold(
         body: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Provider.of<AuthProvider>(context, listen: false)
-                      .login()
-                      .then((value) {
-                    context.go(Home.path);
-                  });
-                },
-                child: const Text("Login"),
+          child: Form(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            key: _formKey,
+            child: Container(
+              width: bodyWidth,
+              padding: EdgeInsets.all(gapUnit),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    App.title,
+                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                        fontWeight: FontWeight.w700, color: textColor),
+                  ),
+                  SizedBox(
+                    height: gapUnit * 4,
+                  ),
+
+                  // 이메일
+                  TextFormField(
+                    controller: emailTextCtrl,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: "이메일",
+                      border: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                    onFieldSubmitted: (value) {
+                      BrickLogger()
+                          .info(msg: 'email - onFieldSubmitted // $value');
+                    },
+                    validator: (value) {
+                      if (value == null || value == '') {
+                        return '이메일을 입력해주세요.';
+                      }
+
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: gapUnit * 2),
+
+                  // 비밀번호
+                  TextFormField(
+                    controller: passwordTextCtrl,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: const InputDecoration(
+                      labelText: "비밀번호",
+                      border: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                    onFieldSubmitted: (value) {
+                      BrickLogger()
+                          .info(msg: 'password - onFieldSubmitted // $value');
+                    },
+                    validator: (value) {
+                      if (value == null || value == '') {
+                        return '비밀번호를 입력해주세요.';
+                      }
+
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: gapUnit * 3),
+
+                  SizedBox(
+                    width: bodyWidth,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Provider.of<AuthProvider>(context, listen: false)
+                            .login()
+                            .then((value) {
+                          context.go(Home.path);
+                        });
+                      },
+                      child: const Text("로그인"),
+                    ),
+                  ),
+                  SizedBox(height: gapUnit),
+
+                  TextButton(
+                    onPressed: () {
+                      context.push(SignUp.path);
+                    },
+                    child: const Text("회원가입"),
+                  ),
+                ],
               ),
-              TextButton(
-                onPressed: () {
-                  context.go(SignUp.path);
-                },
-                child: const Text("회원가입"),
-              ),
-            ],
+            ),
           ),
         ),
       ),

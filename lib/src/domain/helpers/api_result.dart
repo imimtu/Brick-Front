d4f6d9ac471b0b288aa3e8f65a13df9c1bd8ddf2
Kangmin An <hkkm612@gmail.com>
2023-06-01 00:21:11 +0900

    task. [Domain / Helpers] api_result 타입 주석 추가

--- lib/src/domain/helpers/api_result.dart
+++ lib/src/domain/helpers/api_result.dart
@@ -5,6 +5,13 @@ enum ApiErrors {
   invalidInput,
 }
 
+/// Response Value Object
+///
+/// - value's type is [T]. and it is Server's response data type.
+/// It could be [Map<String, dynamic>], [bool], ... .
+///
+/// - msg can be a success message or error message.
+///
 class ApiResult<T> {
   final bool success;
   final ApiErrors? error;

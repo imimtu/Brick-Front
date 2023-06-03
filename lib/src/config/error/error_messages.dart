790440e152f87d4735a3400f169060659e00c958
안강민 <hkkm612@gmail.com>
2023-06-03 19:30:03 +0900

    refactor. [Config / Error] 에러 메시지 접근을 함수로 변경해 명시적 접근 가능하도록 수정

--- lib/src/config/error/error_messages.dart
+++ lib/src/config/error/error_messages.dart
@@ -1,5 +1,8 @@
-import 'package:brick/src/config/error/error_enums.dart';
+library brick.config.error;
+
 import 'package:brick/src/util/brick_logger.dart';
+
+part 'error_enums.dart';
 
 class ErrorMessages {
   static final ErrorMessages _instance = ErrorMessages._internal();
@@ -12,7 +15,19 @@ class ErrorMessages {
     return _instance;
   }
 
-  Map<EnvError, String> env = {
-    EnvError.missingFile: "./assets/env/ 위치에 .env 파일이 존재하지 않습니다.",
-  };
+  /// 환경 변수 관련 에러 메세지
+  String env({required EnvError envError}) {
+    String result = "";
+
+    switch (envError) {
+      case EnvError.invalidEnvProfile:
+        result = "env 입력 값이 올바르지 않습니다. (허용 env: local / develop / release)";
+        break;
+      case EnvError.missingFile:
+        result = "./assets/env/ 위치에 .env 파일이 존재하지 않습니다.";
+        break;
+    }
+
+    return result;
+  }
 }

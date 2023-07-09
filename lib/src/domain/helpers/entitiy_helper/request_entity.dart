193c307937303d51bb256704990be88f49a82f7d
Kangmin An <hkkm612@gmail.com>
2023-07-09 14:05:01 +0900

    fix. [Domain / Helpers / Entity] API 요청 Parameter 전달 시 JSON Encode 진행

--- lib/src/domain/helpers/entitiy_helper/request_entity.dart
+++ lib/src/domain/helpers/entitiy_helper/request_entity.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:brick/src/domain/helpers/entitiy_helper/interfaces/request_value_interface.dart';
 
 class RequestEntity<T extends RequestValueInterface> {
   T value;
 
-  Map<String, dynamic> get params => value.toJson();
+  String get params => jsonEncode(value.toJson());
 
   RequestEntity({
     required this.value,

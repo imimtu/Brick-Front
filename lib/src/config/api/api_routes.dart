2dfec1e7b1010b4f7609692312e4ad4ec38bac3c
안강민 <hkkm612@gmail.com>
2023-08-15 16:37:02 +0900

    create. [Domain / Auth] Token 유효성 검사 로직 생성

--- lib/src/config/api/api_routes.dart
+++ lib/src/config/api/api_routes.dart
@@ -6,5 +6,6 @@ abstract class APIRoutes {
 
   /// Auth
   static const String login = "$baseRouteV1/users/login"; // [POST] 로그인
+  static const String validateToken = "";
   static const String join = "$baseRouteV1/users/join"; // [POST] 회원 가입
 }
--- lib/src/domain/auth/repositories/auth_repository.dart
+++ lib/src/domain/auth/repositories/auth_repository.dart
@@ -1,8 +1,14 @@
 import 'package:brick/src/domain/auth/entities/login/login_request_value.dart';
+import 'package:brick/src/domain/auth/entities/token/valid_token_request_value.dart';
 import 'package:brick/src/domain/helpers/api_requester/api_requester.dart';
 import 'package:brick/src/domain/helpers/entitiy_helper/request_entity.dart';
 
 abstract class AuthRepository {
   /// 로그인
   Future<APIResult> login(RequestEntity<LoginRequestValue> requestEntity);
+
+  /// 토큰 유효성 검사
+  Future<APIResult> validateToken(
+    RequestEntity<ValidateTokenRequestValue> requestEntity,
+  );
 }

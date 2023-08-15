6ed9d188142379f0276e6f0e93b86f3512594c00
안강민 <hkkm612@gmail.com>
2023-08-15 16:38:48 +0900

    create. [Domain / Auth / UseCase] Token 유효성 검사 UseCase 생성

--- lib/src/domain/auth/usecases/auth_usecase.dart
+++ lib/src/domain/auth/usecases/auth_usecase.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
 import 'package:brick/src/domain/auth/entities/login/login_request_value.dart';
-import 'package:brick/src/domain/auth/entities/token_entity.dart';
+import 'package:brick/src/domain/auth/entities/token/token_entity.dart';
+import 'package:brick/src/domain/auth/entities/token/valid_token_request_value.dart';
 import 'package:brick/src/domain/auth/repositories/auth_repository.dart';
 import 'package:brick/src/domain/helpers/api_requester/api_requester.dart';
 import 'package:brick/src/domain/helpers/entitiy_helper/request_entity.dart';
@@ -54,6 +55,50 @@ class AuthUsecase {
       BrickError brickError = BrickError(
         errorMessage: error.toString(),
         errorParameters: loginRequestValue.toJson(),
+      );
+
+      responseEntity = ResponseEntity(isError: true, brickError: brickError);
+    }
+
+    return responseEntity;
+  }
+
+  Future<ResponseEntity<bool>> validateToken({
+    required ValidateTokenRequestValue validateTokenRequestValue,
+  }) async {
+    ResponseEntity<bool> responseEntity;
+
+    try {
+      RequestEntity<ValidateTokenRequestValue> requestEntity =
+          RequestEntity<ValidateTokenRequestValue>(
+        value: validateTokenRequestValue,
+      );
+
+      APIResult<http.Response> result = await remoteRepository
+          .validateToken(requestEntity) as APIResult<http.Response>;
+
+      if (result.success) {
+        var resJson = jsonDecode(result.response!.body);
+
+        bool tokenEntity = resJson['result'];
+
+        responseEntity = ResponseEntity(isError: false, value: tokenEntity);
+      } else {
+        String errMsg = "";
+        errMsg += result.error != null ? "[${result.error?.name}] " : '';
+        errMsg += result.msg ?? "";
+
+        BrickError brickError = BrickError(
+          errorMessage: errMsg == '' ? "" : errMsg,
+          errorParameters: validateTokenRequestValue.toJson(),
+        );
+
+        responseEntity = ResponseEntity(isError: true, brickError: brickError);
+      }
+    } catch (error) {
+      BrickError brickError = BrickError(
+        errorMessage: error.toString(),
+        errorParameters: validateTokenRequestValue.toJson(),
       );
 
       responseEntity = ResponseEntity(isError: true, brickError: brickError);
